bd87dda02ff0df70ccca6ac6fd5f6f91131b1ebd
LoganMiller5505 <loganmiller5505@gmail.com>
2023-01-05 13:48:33 -0500

    Further Grid TeleOp Improvements
    Made the switch between manual and auto improved due to now having support for "first switch loop" code and making manual still work through Roadrunner. Now, need to make a 2d Array of the field that can be used to compare against potential robot movements with the left stick for stacking zones (image of this array's structure sent on discord)

--- BLUE_GRID_TELEOP.java
+++ BLUE_GRID_TELEOP.java
@@ -1,8 +1,13 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 
 
 @TeleOp(name = "GridTeleOp", group = "TeleOp")
@@ -19,7 +24,7 @@ public class BLUE_GRID_TELEOP extends LinearOpMode {
     public void runOpMode(){
 
         ElapsedTime timer = new ElapsedTime();
-        Robot robot = new Robot(this, hardwareMap, telemetry, timer, true);
+        Robot robot = new Robot(this, hardwareMap, telemetry, timer, false);
 
         Delivery delivery = robot.delivery;
 
@@ -36,6 +41,8 @@ public class BLUE_GRID_TELEOP extends LinearOpMode {
         delivery.initEncoders();
 
         int slidePosIdx = 0; //0 = bottom, 1 = low, 2 = med, 3 = high
+
+        boolean firstSwitchLoop = true;
 
 
 
@@ -91,9 +98,23 @@ public class BLUE_GRID_TELEOP extends LinearOpMode {
             switch(drive_mode){
                 case AUTO:
                     if(gamepad1.a && !gamepadAHeld){
-                        drive_mode=DRIVE_MODE.MANUAL;
+
+                        robot.drive.breakFollowing();
+                        robot.drive.setDrivePower(new Pose2d());
+
                         gamepadAHeld = true;
+                        firstSwitchLoop = true;
+
+                        drive_mode=DRIVE_MODE.MANUAL;
+
                     }
+
+                    if(firstSwitchLoop){
+                        robot.drive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                        //Should also update current robot pose here (somehow...)
+                    }
+
+                    firstSwitchLoop = false;
 
                     //May change to be driver oriented rather than field oriented- AKA, integrated into the big ugly switch case below. Shouldn't be necessary, this seems fairly intuitive with some practice
                     if(gamepad1.right_stick_x>0.1){
@@ -153,14 +174,56 @@ public class BLUE_GRID_TELEOP extends LinearOpMode {
                         currentGridX=0;
                     }
 
+                    TrajectorySequence grid = robot.drive.trajectorySequenceBuilder(robot.drive.getPoseEstimate()) //TODO: Make sure we have some way to make getPoseEstimate accurate between switching modes and between auto & teleop
+
+                            .lineTo(new Vector2d(gridToAbsolute(currentGridX), gridToAbsolute(currentGridY)))
+                            .turn(orientationToRadians(currentGridOrientation))
+
+                            .build();
+
+                    robot.drive.followTrajectorySequenceAsync(grid);
+
+                    robot.drive.update();
+
+                    if(!robot.drive.isBusy()){
+                        if(gamepad1.left_stick_x>0.2 && -gamepad1.left_stick_y>0.2){ //Front right
+
+                        }
+                        else if(gamepad1.left_stick_x>0.2 && -gamepad1.left_stick_y<-0.2){ //Back Right
+
+                        }
+                        else if(gamepad1.left_stick_x<-0.2 && -gamepad1.left_stick_y<-0.2){ //Back Left
+
+                        }
+                        else if(gamepad1.left_stick_x<-0.2 && -gamepad1.left_stick_y>0.2){ //Front Left
+
+                        }
+                    }
+
+                    break;
+
+
 
                 case MANUAL:
                     if(gamepad1.a && !gamepadAHeld){
-                        drive_mode=DRIVE_MODE.AUTO;
+
+                        robot.drive.setMotorPowers(0,0,0,0);
+
                         gamepadAHeld = true;
+                        firstSwitchLoop = true;
+
+                        drive_mode=DRIVE_MODE.AUTO;
                     }
 
-                    /*double y = -gamepad1.left_stick_y; // Remember, this is reversed!
+                    if(firstSwitchLoop){
+
+                    }
+
+                    firstSwitchLoop = false;
+
+                    robot.drive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER); //Ideally, would only declare first time case was switched. Look into how to accomplish this
+
+                    double y = -gamepad1.left_stick_y; // Remember, this is reversed!
                     double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
                     double rx = gamepad1.right_stick_x;
 
@@ -182,17 +245,22 @@ public class BLUE_GRID_TELEOP extends LinearOpMode {
                     double backRightPower = (y + x - rx) / denominator;
 
                     if(y<0.075 || x<0.0825 || rx<0.075){ //Account for potential joystick drift
-                        robot.frontLeftM.setPower(frontLeftPower/slow);
+                        /*robot.frontLeftM.setPower(frontLeftPower/slow);
                         robot.backLeftM.setPower(backLeftPower/slow);
                         robot.frontRightM.setPower(frontRightPower/slow);
-                        robot.backRightM.setPower(backRightPower/slow);
+                        robot.backRightM.setPower(backRightPower/slow);*/
+                        robot.drive.setMotorPowers(frontLeftPower/slow, backLeftPower/slow, backRightPower/slow, frontRightPower/slow);
                     }
                     else{
-                        robot.frontLeftM.setPower(0);
+                        /*robot.frontLeftM.setPower(0);
                         robot.frontRightM.setPower(0);
                         robot.backLeftM.setPower(0);
                         robot.backRightM.setPower(0);
-                    }*/
+                         */
+                        robot.drive.setMotorPowers(0,0,0,0);
+                    }
+
+                    break;
 
 
 
@@ -252,5 +320,13 @@ public class BLUE_GRID_TELEOP extends LinearOpMode {
 
         }
 
+    }
+
+    public double gridToAbsolute(int coord){
+        return (coord * 24) + 12;
+    }
+
+    public double orientationToRadians(int orientation){
+        return Math.toRadians(orientation*90);
     }
 }
