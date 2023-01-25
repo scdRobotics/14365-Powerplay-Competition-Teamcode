fda9c8806431db07fbdd8ccd978f729dc8fddd1f
scdrobotics <loganmiller5505@gmail.com>
2023-01-24 19:31:58 -0500

    Update Tracking Wheel Estimates
    Updated lateral distance and forward offset constants

--- drive/StandardTrackingWheelLocalizer.java
+++ drive/StandardTrackingWheelLocalizer.java
@@ -31,8 +31,8 @@ public class StandardTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer 
     public static double WHEEL_RADIUS = 0.6889764; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (encoder) speed
 
-    public static double LATERAL_DISTANCE = 10; // in; distance between the left and right wheels
-    public static double FORWARD_OFFSET = 4; // in; offset of the lateral wheel
+    public static double LATERAL_DISTANCE = 13.5; // in; distance between the left and right wheels
+    public static double FORWARD_OFFSET = 7.25; // in; offset of the lateral wheel
 
     public static double X_MULTIPLIER = 1; // Multiplier in the X direction
     public static double Y_MULTIPLIER = 1; // Multiplier in the Y direction
